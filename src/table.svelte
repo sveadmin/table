49d24182accc9ce8db911223de7170cab0063985
Lord_G <marton.kufferstein@gmail.com>
2023-04-09 20:10:35 +0200

    Making sense of typescript setup

--- src/table.svelte
+++ src/table.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-  import { DropDownMultiSelect } from '@sveadmin/element'
+  import { CheckboxSwitch } from '@sveadmin/element'
 
-  const values = [
-    {id: 1, value: 'Test'},
-    {id: 5, value: 'Test123123'},
-  ]
+  // const values = [
+  //   {id: 1, value: 'Test'},
+  //   {id: 5, value: 'Test123123'},
+  // ]
 
-  const getSelection = () => {return {
-    5: 'asdasdasd'
-  }}
+  // const getSelection = () => {return {
+  //   5: 'asdasdasd'
+  // }}
 </script>
 
 
 <h1>This is Svelte 3</h1>
-<DropDownMultiSelect {getSelection} {values}/>
+<!-- <DropDownMultiSelect {getSelection} {values}/> -->
+<CheckboxSwitch />
 
 <style>
   :global(h1) {
